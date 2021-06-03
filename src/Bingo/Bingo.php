8b4c51c7ba9cefadd688446bc20951894a96750c
moises <correodemomuar@gmail.com>
2021-06-03 14:16:37 +0200

    metodo get carton

--- src/Bingo/Bingo.php
+++ src/Bingo/Bingo.php
@@ -8,24 +8,50 @@ class Bingo implements BingoableInterface{
     private string $nombre2;
     private string $nombre3;
     private string $nombre4;
+    private array $totalcartones;
 
     public function __construct(string $nombre1, string $nombre2, string $nombre3, string $nombre4){
         $this-> nombre1 = $nombre1;
         $this-> nombre2 = $nombre2;
         $this-> nombre3 = $nombre3;
         $this-> nombre4 = $nombre4;
+        $this-> fichero = fopen('cartones.dat', 'rb');
     }
     public function getCarton(){
-        $fichero = fopen('cartones.dat', 'rb');
-        $linea = fgets($fichero);
-        $carton[] = explode( ".", $linea );
+        $linea = fgets($this-> fichero);
+        $carton = [];
+        $carton = explode( ".", $linea );
+        $this-> totalcartones[] = $carton;
+        echo "<table border=1px> <tr>";
+        $cont = 0;
+        var_dump($carton);
+        foreach( $carton as $clave => $valor ){
+            if($valor == "_"){
+                echo "<td>"." "."</td>";
+            }
+            else{
+                echo "<td>".$valor."</td>";
+            }
+            if($cont==8 || $cont==17){
+                echo "</tr><tr>";
+            }
+            $cont++;
+        }
+        echo "</tr></table>";
+    }
+
+    public function verifica(){
+        $numeroGetBola = 22;
+        foreach ($this->totalcartones as $key => $value) {
         
-        foreach($carton as $clave => $valor){
-            echo "<li> $valor <li>";
+            foreach ($this->totalcartones[$key] as $key => $value) {
+                echo "$value</br>";
+            }
         }
     }
+    
     public function getBola(){
-
+        
     }
     public function setCarton(){
 
@@ -34,7 +60,7 @@ class Bingo implements BingoableInterface{
 
     }
     public function getJugadores(){
-        heloo;
+       
     }
 
 }
